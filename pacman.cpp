2cd7f14617fbeb19b4e788842f3620f7f3cbdcbb
Jay <j7nj1.t41@gmail.com>
2023-11-08 13:39:08 -0400

    Set up window init and structure

--- pacman.cpp
+++ pacman.cpp
@@ -1,17 +1,58 @@
-#include <iostream>
 #include <ncurses.h>
+
+#include <chrono>
+#include <thread>
+
+class Game
+{
+private:
+    static const int screenY{30};
+    static const int screenX{80};
+    int maxY{};
+    int maxX{};
+    WINDOW* gameW{};
+
+public:
+    ~Game() { delwin(gameW); }
+
+    // getters
+    WINDOW* getGameW() { return gameW; }
+    const int& getScreenY() { return screenY; }
+    const int& getScreenX() { return screenX; }
+
+    void setScreen()
+    {
+        getmaxyx(stdscr, maxY, maxX);
+    }
+
+    void setGameW()
+    {
+        gameW = newwin(screenY, screenX, (maxY - screenY) / 2, (maxX - screenX) / 2);
+    }
+};
+
+void gameInit(Game& game)
+{
+    // set up ncurses
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);    
+    refresh();
+
+    game.setScreen();
+    game.setGameW();
+
+    box(game.getGameW(), 0, 0);
+    wrefresh(game.getGameW());
+}
 
 int main()
 {
-    initscr();
+    Game game{};
+    gameInit(game); 
 
-    raw();
-    noecho();
-    curs_set(0);
-    refresh();
 
-    wprintw(stdscr, "Hello World!");
-    
     getch();
     endwin();
     return 0;
